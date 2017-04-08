fbb758df51bca3397011f42295b5bc4a2edfec74
litfb <litfb@yonyou.com>
2017-04-08 15:47:48 +0800

    1

---
+++ src/com/shadow/test/TestCalendar.java
@@ -0,0 +1,12 @@
+package com.shadow.test;
+
+import java.util.Calendar;
+
+public class TestCalendar {
+
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2016);
+	}
+
+}
